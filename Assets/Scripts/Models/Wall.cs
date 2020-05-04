7fb82ea5433189b32e4d5577309cd710c9d59d98
MyAppPlay <50810271+MyAppPlay@users.noreply.github.com>
2020-05-04 23:25:31 +0300

    SelectionController

---
+++ Assets/Scripts/Models/Wall.cs
@@ -0,0 +1,10 @@
+﻿namespace SecondAttempt
+{
+    public sealed class Wall : BaseObjectScene, ISelectObject
+    {
+        public string GetMessage()
+        {
+            return name; //закешировать
+        }
+    } 
+}
